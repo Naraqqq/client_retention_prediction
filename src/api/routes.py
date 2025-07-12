efd8409eb137eee5e37faa67b46bf41a631ff3cd
Hoang <work.nguyen02@gmail.com>
2025-07-12 08:43:30 +0300

    add readme

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import Session
 from src.database.connection import get_db
 from src.ML.predictor import predict
 from src.database.crud import getCustomer, savePrediction
-
+import json
+import os
 
 router = APIRouter()
 
@@ -13,14 +14,23 @@ router = APIRouter()
 def predict_retention(customerid : float, db : Session = Depends(get_db)):
     df = getCustomer(customerid, db)
 
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    json_path = os.path.join(BASE_DIR, '../../models/best_treshold.json')
+
+    with open(json_path, 'r') as f:
+        threshold = json.load(f)
+
     if df is None or df.empty:
         raise HTTPException(status_code=404, detail="Customer not found or no data available")
 
     prediction = float(predict(df))
     savePrediction(customerid, prediction, db)
+    message = ("АХТУНГ КЛИЕНТ УТЕКАЕТ НЕМЕДЛЕННО ДАТЬ КОШКОЖЕНА"
+               if prediction > threshold else "ЭТОТ ПУПСИК С НАМИ ДОИМ ДАЛЬШЕ")
 
     if prediction is None:
         return None
     else:
         return {'customerid' : customerid,
-                'prediction' : prediction}
+                'prediction' : prediction,
+                'message' : message}
